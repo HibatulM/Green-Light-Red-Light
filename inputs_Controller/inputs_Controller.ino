a5e9a016b83aae465653b943e07adb22ef02e35a
Hibatul Muqeet <161466064+HibatulM@users.noreply.github.com>
2026-04-27 01:57:41 -0500

    adding serial logic
    Adding serial and data sharing

--- inputs_Controller/inputs_Controller.ino
+++ inputs_Controller/inputs_Controller.ino
@@ -1,19 +1,5 @@
 //Inputs code 
 struct Components {
-  //StartBtn will tell us if the game has started or not 
-  //if startBtn is 0 then game has not started
-  //if startBtn is 1 then game has started
-  //if startBrn is 2 then game has been forcefully reset
-  //StartBtn is to be ONLY WRITE in this file, the output file 
-  // will be the one reading this varibale 
-  int startBtn;
-  // moveBtn will tell us if the player is moving 
-  //or staying still 
-  //If moveBtn is 0 the player is not moving 
-  //If moveBtn is 1 then player is moving 
-  //This variable is also to be ONLY WRITE and the outputs file
-  //will read this variable to calculate the score
-  int moveBtn;
   //This will have the score recorded depending on the states of 
   //the LEDs. 
   //Score will be ONLY READ in this file, the outputs file will 
@@ -27,18 +13,100 @@ struct Components {
   //The gameStatus is READ only 
   int gameStatus;
 
-}
+};
 
-Components input;
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+const int btnOnePin;
+const int btnTwoPin;
+
+int gameState = 0;
+int localScore = 0;
+
+int btnOnePrevState = LOW;
+int btnOneState = LOW;
+int delay = 50;
+unsigned long btnOneLastDebounceTime = 0;
+
+unsigned long btnTwoLastDebounceTime = 0;
+int mvDelay = 5;
+int btnTwoPrevState = LOW;
+int btnTwoState = LOW;
+
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(btnOnePin, INPUT);
+  pinMode(btnTwoPin, INPUT);
   Serial.begin(9600);
-  
-  input.moveBtn = 0;
-  input.startBtn = 0;
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.print("Press START");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+  //Serial Communication
+  Components data;
+  data.gameStatus = gameState;
+  data.score = localScore;
+
+  Serial.write((byte*)&data, sizeof(data));
+
+
+
+  //Reading and handling Inputs
+  int btnOneRead = digitalRead(btnOnePin);
+  int btnTwoRead = digitalRead(btnTwoPin);
+
+  if(btnOneRead != btnOnePrevState){
+    btnOneLastDebounceTime = millis();
+  }
+  if((millis() - btnOneLastDebounceTime) > delay){
+    if(btnOneRead != btnOneState){
+      btnOneState = btnOneRead;
+      if(btnOneState == HIGH) {
+        if(gameState == 0){
+          gameState = 1;
+        } 
+        else if(gameState == 1){
+          gameState = 2;
+        }
+        else if(gameState == 2){
+          gameState = 0;
+        }
+      }
+    }
+    btnOnePrevState = btnOneRead;
+  }
+
+
+  if(gameState == 0){
+    lcd.setCursor(0, 0);
+    lcd.print("PRESS START");
+  }
+  if (gameState == 1){
+    lcd.print("Score: ");
+    lcd.setCursor(0, 8);
+    lcd.print(localScore);
+  }
+  if(localScore == 100){
+    lcd.setCursor(0, 0);
+    lcd.print("                ");
+    lcd.print("YOU WON :) !");
+  }
+  if(gameState == 2){
+    lcd.setCursor(0, 0);
+    lcd.print("                ");
+    lcd.print("NEW GAME...");
+  }
+  if(gameState == 3){
+    lcd.setCursor(0, 0);
+    lcd.print("                ");
+    lcd.print("PLAY AGAIN");
+  }
+
 
 }
--- outputs_controller/outputs_controller.ino
+++ outputs_controller/outputs_controller.ino
@@ -1,19 +1,5 @@
 //Outputs Code 
 struct Components {
-  //StartBtn will tell us if the game has started or not 
-  //if startBtn is 0 then game has not started
-  //if startBtn is 1 then game has started
-  //if startBrn is 2 then game has been forcefully reset
-  //StartBtn is to be ONLY READ in this file, the input file 
-  // will be the one writing to varibale 
-  int startBtn;
-  // moveBtn will tell us if the player is moving 
-  //or staying still 
-  //If moveBtn is 0 the player is not moving 
-  //If moveBtn is 1 then player is moving 
-  //This variable is also to be ONLY READ and only the inputs file
-  //will write to this variable
-  int moveBtn;
   //This will have the score recorded depending on the states of 
   //the LEDs. 
   //Score will be ONLY WRITE in this file, the inputs file will 
@@ -28,12 +14,73 @@ struct Components {
 
 }
 
+//buzzer Frequencies
+#define NOTE_C4 262
+#define NOTE_D4 294
+#define NOTE_E4 330
+#define NOTE_F4 349
+#define NOTE_G4 392
+#define NOTE_A4 440
+#define NOTE_B4 494
+#define NOTE_AS4 466
+#define NOTE_C5 523
+
+// "Happy Birthday" melody sequence
+int melody[] = {
+  NOTE_C4, NOTE_C4, NOTE_D4, NOTE_C4, NOTE_F4, NOTE_E4, // "Happy Birthday to You"
+  NOTE_C4, NOTE_C4, NOTE_D4, NOTE_C4, NOTE_G4, NOTE_F4, // "Happy Birthday to You"
+  NOTE_C4, NOTE_C4, NOTE_C5, NOTE_A4, NOTE_F4, NOTE_E4, NOTE_D4, // "Happy Birthday dear [Name]"
+  NOTE_AS4, NOTE_AS4, NOTE_A4, NOTE_F4, NOTE_G4, NOTE_F4 // "Happy Birthday to You"
+};
+
+
+// Duration of each note (4 = quarter note, 2 = half note, etc.)
+int noteDurations[] = {
+  4, 4, 4, 4, 4, 2,
+  4, 4, 4, 4, 4, 2,
+  4, 4, 4, 4, 4, 4, 2,
+  4, 4, 4, 4, 4, 2
+};
+
+
+// "Twinkle twinle little star"
+int melody2[] = {
+  NOTE_C4, NOTE_C4, NOTE_G4, NOTE_G4, NOTE_A4, NOTE_A4, NOTE_G4,  // Twinkle, twinkle, little star
+  NOTE_F4, NOTE_F4, NOTE_E4, NOTE_E4, NOTE_D4, NOTE_D4, NOTE_C4,  // How I wonder what you are
+  NOTE_G4, NOTE_G4, NOTE_F4, NOTE_F4, NOTE_E4, NOTE_E4, NOTE_D4,  // Up above the world so high
+  NOTE_G4, NOTE_G4, NOTE_F4, NOTE_F4, NOTE_E4, NOTE_E4, NOTE_D4,  // Like a diamond in the sky
+  NOTE_C4, NOTE_C4, NOTE_G4, NOTE_G4, NOTE_A4, NOTE_A4, NOTE_G4,  // Twinkle, twinkle, little star
+  NOTE_F4, NOTE_F4, NOTE_E4, NOTE_E4, NOTE_D4, NOTE_D4, NOTE_C4   // How I wonder what you are}
+};
+
+int noteDurations2[] = {
+  4, 4, 4, 4, 4, 4, 2,  // Twinkle, twinkle, little star (G4 is half note/longer)
+  4, 4, 4, 4, 4, 4, 2,  // How I wonder what you are
+  4, 4, 4, 4, 4, 4, 2,  // Up above the world so high
+  4, 4, 4, 4, 4, 4, 2,  // Like a diamond in the sky
+  4, 4, 4, 4, 4, 4, 2,  // Twinkle, twinkle, little star
+  4, 4, 4, 4, 4, 4, 2   // How I wonder what you are
+};
+
+
 const int greenPin;
 const int redPin;
 const int yellowPin;
 const int buzzerPin;
 
-Components output;
+
+int greenState = LOW;
+int redState = LOW;
+int yellowState = LOW;
+
+int gameSts = 0;
+int randNum = 0;
+
+unsigned long prevMillis = 0;
+unsigned long interval = 1000;
+
+int localScore = 0;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -41,11 +88,89 @@ void setup() {
   pinMode(greenPin, OUTPUT);
   pinMode(yellowPin, OUTPUT);
   pinMode(buzzerPin, OUTPUT);
-  output.gameStatus = 0;
-  output.score = 0;
+  
 }
 
 void loop() {
+  randNum = rand() % 11;
+  unsigned long currMillis = millis();
   // put your main code here, to run repeatedly:
+  Components data;
+  if(curMillis - previousMillis >= interval){
+      if (Serial.available() >= sizeof(data)) {
+      Serial.readBytes((byte*)&data, sizeof(data));
 
+      // Use your data
+      gameSts = data.gameStatus;
+      // int score  = data.score;
+    }
+    if(gameSts == 1 && ((redState == LOW && greenState == HIGH) || (redState == HIGH && greenState == LOW))){
+      if(redState == HIGH && localScore < data.score){
+        gameSts = 3;
+      }
+    }
+    if(data.score >= 100 && gameSts == 1){
+      //paly winning tune
+      for (int thisNote = 0; thisNote < 25; thisNote++) {
+        // Calculate the duration of each note in milliseconds
+        // A quarter note is 1000/4 = 250ms
+        int noteDuration = 1000 / noteDurations[thisNote];
+        tone(buzzerPin, melody[thisNote], noteDuration);
+
+        // Add a small pause between notes to make them distinct
+        int pauseBetweenNotes = noteDuration * 1.30;
+        delay(pauseBetweenNotes);
+
+        // Stop the tone before moving to the next note
+        noTone(buzzerPin);
+    } 
+    }
+    prevMillis = currMillis;
+  }
+
+  if(curMillis - prevMillis >= interval+(2000*randNum)){
+    if(gameSts == 1){
+      if(randNum % 2 == 0){
+        digitalWrite(greenPin, HIGH);
+        greenState = HIGH;
+        digitalWrite(redPin, LOW);
+        redState = LOW;
+        localScore = data.score;
+      }else{
+        digitalWrite(redPin, HIGH);
+        redState = HIGH;
+        digitalWrite(greenPin, LOW);
+        greenState = LOW;
+        localScore = data.score;
+      }
+      if(randNum % 3 == 0){
+        digitalWrite(yellowPin, HIGH);
+        yellowState = HIGH;
+      }else{
+        digitalWrite(yellowPin, LOW);
+        yellowState = LOW;
+      }
+      if(randNum % 4 == 0){
+        //play the twinkle twinkle little star tune from buzzer
+        for (int thisNote = 0; thisNote < 42; thisNote++) {  // 42 notes total
+          // Calculate note duration: 1 second divided by note type (e.g., quarter note = 1000/4 = 250ms)
+          int noteDuration = 1000 / noteDurations[thisNote];
+          
+          // Play the note on the buzzer
+          tone(buzzerPin, melody[thisNote], noteDuration);
+          
+          // Add a small pause between notes to make them sound distinct
+          // A 30% pause (multiply by 1.3) is a good starting point
+          int pauseBetweenNotes = noteDuration * 1.3;
+          delay(pauseBetweenNotes);
+          
+          // Stop the tone before moving to the next note
+          noTone(buzzerPin);
+        }
+      }
+    }
+    prevMillis = currMillis;
+  }
+
+  
 }
