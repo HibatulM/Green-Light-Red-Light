cadc3d823dae20e54d25199b714fe79a85e3aadc
Hibatul Muqeet <161466064+HibatulM@users.noreply.github.com>
2026-04-27 13:45:30 -0500

    all working code

--- inputs_Controller/inputs_Controller.ino
+++ inputs_Controller/inputs_Controller.ino
@@ -1,33 +1,11 @@
 //----------------------------Header--------------------------------------------
 //Name: Hibatul Muqeet, Yash Rathore
 //Project: Traffic Light: A Fun Interactive Reaction Game 
-//Abstract: This project presents a digital simulation of the classic kindergarten game,
-// Red Light Green Light, implemented using multiple Arduino microcontrollers. 
-// The system consists of a network of player-controlled push buttons and a centralized light controller,
-//  all coordinated through a master controller that manages game logic and scoring. 
-// Inter-device communication is handled via serial communication between the Arduino units,
-//  enabling real-time responsiveness to player actions. Game progress and scores are 
-// displayed to all participants through an integrated LCD display. 
-// The result is an adaptive, fully responsive digital recreation of Red Light Green Light that faithfully 
-// replicates the original game's mechanics while demonstrating robust, expert-level 
-// embedded systems design and game management.
 
-
-//Inputs code 
 #include <LiquidCrystal.h>
 
 struct Components {
-  //This will have the score recorded depending on the states of 
-  //the LEDs. 
-  //Score will be ONLY READ in this file, the outputs file will 
-  //change this score and we will display this value on the LCD 
   int score;
-  //This gameStatus will tell us if the player has won or lost 
-  //If gameStatus is 0 then game is ongoing, no win or loss 
-  //If gameStatus is 1 the game is over 
-  //The gameStaus will only be 1 if score is >= 100 
-  //This gameStatus will only be 2 if the palyer lost 
-  //The gameStatus is READ only 
   int gameStatus;
   int btnState;
 };
@@ -55,29 +33,29 @@ int debounceDelay = 50;
 int mvDelay = 5;
 
 unsigned long prevMillis = 0;
-unsigned long lastSerialRecieve = 0;
+unsigned long lastSerialReceive = 0;
 unsigned long lastSerialSend = 0;
-unsigned long serialInterval  = 50;
+unsigned long serialInterval = 50;  // Send every 50ms (20 times per second)
 unsigned long nextGameInterval = 3000;
 unsigned long lastGameOver = 0;
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(btnOnePin, INPUT_PULLUP);
   pinMode(btnTwoPin, INPUT_PULLUP);
   Serial.begin(9600);
 
   lcd.begin(16, 2);
-  lcd.setCursor(0, 0);
-  lcd.print("  Press START  ");
+  updateDisplay();
 }
 
 void updateDisplay(){
-  lcd.print("                ");
-
+  lcd.clear();
+  
   if(gameState == 0){
     lcd.setCursor(0, 0);
     lcd.print("  Press START  ");
+    lcd.setCursor(0, 1);
+    lcd.print("                ");
   } else if (gameState == 1){
     lcd.setCursor(0, 0);
     lcd.print("Score: ");
@@ -89,16 +67,17 @@ void updateDisplay(){
     } else {
       lcd.print("Almost there!  ");
     }
-  }else if (gameState == 2){
+  } else if (gameState == 2){
     lcd.setCursor(0, 0);
     lcd.print("   GAME OVER   ");
     lcd.setCursor(0, 1);
     lcd.print("  PLAY AGAIN!  ");
     
     if(millis() - lastGameOver >= nextGameInterval){
-      gameState = 0;  // play again state
+      gameState = 0;
       localScore = 0;
       lastGameOver = millis();
+      updateDisplay();
     }
     
   } else if (gameState == 3){
@@ -108,9 +87,10 @@ void updateDisplay(){
       lcd.setCursor(0, 1);
       lcd.print("  YOU LOST!    ");
       if(millis() - lastGameOver >= nextGameInterval){
-        gameState = 0;  // play again state
+        gameState = 0;
         localScore = 0;
         lastGameOver = millis();
+        updateDisplay();
       }
     } else {
       lcd.setCursor(0, 0);
@@ -118,19 +98,19 @@ void updateDisplay(){
       lcd.setCursor(0, 1);
       lcd.print("  CONGRATS!    ");
       if(millis() - lastGameOver >= nextGameInterval){
-        gameState = 0;  // play again state
+        gameState = 0;
         localScore = 0;
         lastGameOver = millis();
+        updateDisplay();
       }
     }
   }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   unsigned long currMillis = millis();
   
-  //Reading and handling Inputs
+  // Reading and handling Inputs
   int btnOneRead = digitalRead(btnOnePin);
   int btnTwoRead = digitalRead(btnTwoPin);
 
@@ -143,18 +123,22 @@ void loop() {
       if(btnOneState == LOW) {
         if(gameState == 0){
           gameState = 1;
+          updateDisplay();
+          
         } 
         else if(gameState == 1){
           gameState = 2;
+          updateDisplay();
         }
         else if(gameState == 2){
           gameState = 0;
+          updateDisplay();
         }
       }
     }
     
   }
-  btnOneState = btnOneRead;
+  btnOnePrevState = btnOneRead;
 
   if(btnTwoRead != lastBtnTwoState){
     btnTwoLastDebounceTime = millis();
@@ -165,38 +149,37 @@ void loop() {
     }
     
   }
-  btnTwoState = btnTwoRead;
+  lastBtnTwoState = btnTwoRead;
 
-  if(currMillis - prevMillis >= 1000){
-
-    if(currMillis - lastSerialSend >= serialInterval){
-        //Serial Communication
-      Components data;
-      data.gameStatus = gameState;
-      data.score = localScore;
-      if(btnTwoState == LOW){
-        data.btnState = 1;
-      } else {
-        data.btnState = 0;
-      }
-
-      Serial.write((byte*)&data, sizeof(data));
-      lastSerialSend = currMillis;
+  // REMOVED the 1-second delay - now sends continuously
+  // Serial Communication - Send every 50ms
+  if(currMillis - lastSerialSend >= serialInterval){
+    Components data;
+    data.gameStatus = gameState;
+    data.score = localScore;
+    if(btnTwoState == LOW){
+      data.btnState = 1;
+    } else {
+      data.btnState = 0;
     }
-    
-    if(Serial.available() >= sizeof(Components)){
-      Components updateData;
-      Serial.readBytes((byte*)&updateData, sizeof(updateData));
 
-      gameState = updateData.gameStatus;
-      prevScore = localScore;
-      localScore = updateData.score;
-    }
+    Serial.write((byte*)&data, sizeof(data));
+    lastSerialSend = currMillis;
+  }
+  
+  // Receive data from output Arduino
+  if(Serial.available() >= sizeof(Components)){
+    Components updateData;
+    Serial.readBytes((byte*)&updateData, sizeof(updateData));
+
+    gameState = updateData.gameStatus;
+    prevScore = localScore;
+    localScore = updateData.score;
     
     if(localScore != prevScore){
       updateDisplay();
+      Serial.println(localScore);
       prevScore = localScore;
     }
   }
-  prevMillis = currMillis;
 }
--- outputs_controller/outputs_controller.ino
+++ outputs_controller/outputs_controller.ino
@@ -1,18 +1,7 @@
 //Outputs Code 
 struct Components {
-  //This will have the score recorded depending on the states of 
-  //the LEDs. 
-  //Score will be ONLY WRITE in this file, the inputs file will 
-  //read this score and display on the LCD 
   int score;
-  //This gameStatus will tell us if the player has won or lost 
-  //If gameStatus is 0 then game is ongoing, no win or loss 
-  //If gameStatus is 1 the game is over 
-  //The gameStaus will only be 1 if score is >= 100 
-  //This gameStatus will only be 2 if the palyer lost 
   int gameStatus;
-
-  //Help us track player movements 
   int btnState;
 };
 
@@ -33,14 +22,12 @@ struct Components {
 
 // "Happy Birthday" melody sequence
 int winMelody[] = {
-  NOTE_C4, NOTE_C4, NOTE_D4, NOTE_C4, NOTE_F4, NOTE_E4, // "Happy Birthday to You"
-  NOTE_C4, NOTE_C4, NOTE_D4, NOTE_C4, NOTE_G4, NOTE_F4, // "Happy Birthday to You"
-  NOTE_C4, NOTE_C4, NOTE_C5, NOTE_A4, NOTE_F4, NOTE_E4, NOTE_D4, // "Happy Birthday dear [Name]"
-  NOTE_AS4, NOTE_AS4, NOTE_A4, NOTE_F4, NOTE_G4, NOTE_F4 // "Happy Birthday to You"
+  NOTE_C4, NOTE_C4, NOTE_D4, NOTE_C4, NOTE_F4, NOTE_E4,
+  NOTE_C4, NOTE_C4, NOTE_D4, NOTE_C4, NOTE_G4, NOTE_F4,
+  NOTE_C4, NOTE_C4, NOTE_C5, NOTE_A4, NOTE_F4, NOTE_E4, NOTE_D4,
+  NOTE_AS4, NOTE_AS4, NOTE_A4, NOTE_F4, NOTE_G4, NOTE_F4
 };
 
-
-// Duration of each note (4 = quarter note, 2 = half note, etc.)
 int winDurations[] = {
   4, 4, 4, 4, 4, 2,
   4, 4, 4, 4, 4, 2,
@@ -48,34 +35,14 @@ int winDurations[] = {
   4, 4, 4, 4, 4, 2
 };
 
-
-// "Twinkle twinle little star"
-int melody2[] = {
-  NOTE_C4, NOTE_C4, NOTE_G4, NOTE_G4, NOTE_A4, NOTE_A4, NOTE_G4,  // Twinkle, twinkle, little star
-  NOTE_F4, NOTE_F4, NOTE_E4, NOTE_E4, NOTE_D4, NOTE_D4, NOTE_C4,  // How I wonder what you are
-  NOTE_G4, NOTE_G4, NOTE_F4, NOTE_F4, NOTE_E4, NOTE_E4, NOTE_D4,  // Up above the world so high
-  NOTE_G4, NOTE_G4, NOTE_F4, NOTE_F4, NOTE_E4, NOTE_E4, NOTE_D4,  // Like a diamond in the sky
-  NOTE_C4, NOTE_C4, NOTE_G4, NOTE_G4, NOTE_A4, NOTE_A4, NOTE_G4,  // Twinkle, twinkle, little star
-  NOTE_F4, NOTE_F4, NOTE_E4, NOTE_E4, NOTE_D4, NOTE_D4, NOTE_C4   // How I wonder what you are}
-};
-
-int loseDurations[] = {
-  4, 4, 4, 4, 4, 4, 2,  // Twinkle, twinkle, little star (G4 is half note/longer)
-  4, 4, 4, 4, 4, 4, 2,  // How I wonder what you are
-  4, 4, 4, 4, 4, 4, 2,  // Up above the world so high
-  4, 4, 4, 4, 4, 4, 2,  // Like a diamond in the sky
-  4, 4, 4, 4, 4, 4, 2,  // Twinkle, twinkle, little star
-  4, 4, 4, 4, 4, 4, 2   // How I wonder what you are
-};
-
 int loseMelody[] = {
-  NOTE_C4, NOTE_DS4, NOTE_G3,  // Descending minor pattern
+  NOTE_C4, NOTE_DS4, NOTE_G3,
   NOTE_F3, NOTE_GS3, NOTE_C4
 };
 
-int loseDurations3[] = {
-  4, 8, 4,  // First phrase
-  4, 8, 2   // Second phrase, ending slightly longer
+int loseDurations[] = {
+  4, 8, 4,
+  4, 8, 2
 };
 
 const int greenPin = 7;
@@ -83,14 +50,11 @@ const int redPin = 6;
 const int yellowPin = 5;
 const int buzzerPin = 4;
 
-
 int greenState = LOW;
 int redState = LOW;
 int yellowState = LOW;
 
-
-//record changes in the game 
-int gameSts = 0; //0:game not startedm, 1:running, 2:game reset, 3=game over(win or loss)
+int gameSts = 0;
 int randNum = 2;
 int score = 0;
 int localScore = 0;
@@ -103,14 +67,13 @@ unsigned long lastDebounce = 0;
 
 unsigned long prevMillis = 0;
 unsigned long interval = 1000;
-unsigned long ledInterval = 2000 * (randNum);
+unsigned long ledInterval = 2000;  // Changed: LED changes every 2 seconds
 unsigned long scoreInterval = 500;
 unsigned long serialInterval = 50;
 
 int sucRead = 0;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
@@ -145,64 +108,83 @@ void playWinTune(){
 }
 
 void updateLed(){
-  if(gameSts == 1){
-    randNum = random(0, 10);
-    if (randNum % 2 == 0) {
-      digitalWrite(greenPin, HIGH);
-      digitalWrite(redPin, LOW);
-      greenState = HIGH;
-      redState = LOW;
-    } else {
-      digitalWrite(redPin, HIGH);
-      digitalWrite(greenPin, LOW);
-      redState = HIGH;
-      greenState = LOW;
-    }
-    
-    // Yellow LED pattern
-    if (randNum % 3 == 0) {
-      digitalWrite(yellowPin, HIGH);
-      yellowState = HIGH;
-    } else {
-      digitalWrite(yellowPin, LOW);
-      yellowState = LOW;
-    }
+  // Generate new random pattern
+  randNum = random(0, 10);
+  
+  // Set Red/Green LEDs (mutually exclusive)
+  if (randNum % 2 == 0) {
+    digitalWrite(greenPin, HIGH);
+    digitalWrite(redPin, LOW);
+    greenState = HIGH;
+    redState = LOW;
+  } else {
+    digitalWrite(redPin, HIGH);
+    digitalWrite(greenPin, LOW);
+    redState = HIGH;
+    greenState = LOW;
   }
-
+  
+  // Set Yellow LED
+  if (randNum % 3 == 0) {
+    digitalWrite(yellowPin, HIGH);
+    yellowState = HIGH;
+  } else {
+    digitalWrite(yellowPin, LOW);
+    yellowState = LOW;
+  }
 }
 
 void loop() {
-  // randNum = rand() % 11;
   unsigned long currMillis = millis();
-  // put your main code here, to run repeatedly:
+  
   Components data;
   if (Serial.available() >= sizeof(data)) {
     Serial.readBytes((byte*)&data, sizeof(data));
     sucRead = 1;
-    // Use your data
+    int oldGameSts = gameSts;
+
     gameSts = data.gameStatus;
     lastBtnState = data.btnState;
-    score  = data.score;
-  }else{
+    score = data.score;
+    localScore = data.score;
+    updateLed();
+
+    if (oldGameSts == 0 && gameSts == 1) {
+      score = 0;
+      localScore = 0;
+      lastLedChange = millis();
+      updateLed();   // turn LED on immediately when START is clicked
+    }
+  } else {
     sucRead = 0;
   }
-  if(gameSts == 1 && ((redState == LOW && greenState == HIGH) || (redState == HIGH && greenState == LOW))){
-    if(redState == HIGH && sucRead == 1 && localScore < data.score){
-      gameSts = 3;
-    }
-  }
-  if(data.score >= 100 && gameSts == 1){
-    //paly winning tune
-    playWinTune();
-  }
-  else if(gameSts == 3 and data.score != 100){
+  
+  // Check for cheating (moving on red)
+  if(gameSts == 1 && redState == HIGH && sucRead == 1 && localScore < data.score && lastBtnState == HIGH){
+    gameSts = 2;  // Game over - loss
+    digitalWrite(greenPin, LOW);
+    digitalWrite(redPin, LOW);
+    digitalWrite(yellowPin, LOW);
     playLoseTune();
   }
+  
+  if(data.score >= 100 && gameSts == 1){
+    playWinTune();
+    gameSts = 3;
+  }
+  else if(gameSts == 2 && data.score != 100){
+    playLoseTune();
+    gameSts = 3;
+  }
+  
   else if(gameSts == 1){
+    // FIXED: Only update LED when enough time has passed (every 2 seconds)
     if(currMillis - lastLedChange >= ledInterval){
       updateLed();
       lastLedChange = currMillis;
     }
+    
+    // Score increment logic - only when button is pressed AND light is green
     if(lastBtnState == HIGH && greenState == HIGH){
       if(currMillis - lastScoreUpdate >= scoreInterval){
         if(score < 100){
@@ -210,16 +192,16 @@ void loop() {
           lastScoreUpdate = currMillis;
         }
       }
-    } else {
-      lastScoreUpdate = currMillis;
-    }
-
-    if (lastBtnState == HIGH && redState == HIGH) {
-      gameSts = 2;  // Game over - loss
+    } else if(lastBtnState == HIGH && redState == HIGH) {
+      // Moving on red - instant loss
+      gameSts = 2;
       digitalWrite(greenPin, LOW);
       digitalWrite(redPin, LOW);
       digitalWrite(yellowPin, LOW);
       playLoseTune();
+    } else {
+      // Reset score update timer when not actively scoring
+      lastScoreUpdate = currMillis;
     }
 
     if (score >= 100) {
@@ -231,13 +213,13 @@ void loop() {
     }
   }
 
+  // Send data back to input Arduino
   if(currMillis - lastSerialSend >= serialInterval){
     Components updateData;
     updateData.gameStatus = gameSts;
     updateData.score = score;
+    updateData.btnState = lastBtnState;
     Serial.write((byte*)&updateData, sizeof(updateData));
     lastSerialSend = currMillis;
   }
-
-  
 }
